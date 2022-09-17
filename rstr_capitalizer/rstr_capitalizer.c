c00143198dfc29dd4ffc7c6c3b68976af80707d8
ahmetcanfakili <ahmetcanfakili50@gmail.com>
2022-09-17 16:09:36 +0300

    updated

--- rstr_capitalizer/rstr_capitalizer.c
+++ rstr_capitalizer/rstr_capitalizer.c
@@ -3,25 +3,17 @@
 void str_capitalizer(char *str)
 {
     int i = 0;
-    if (str[i] >= 'a' && str[i] <= 'z')
-    {
-		str[i] -= 32;
-        write(1, &str[i], 1);
-        i++;
-    }
-    
     while(str[i])
     {
         if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] += 32;
-
-        if (str[i] >= 'a' && str[i] <= 'z' && (str[i-1] == ' ' || str[i-1] == '\t'))
+        
+        if (str[i] >= 'a' && str[i] <= 'z' && (str[i+1] == ' ' || str[i+1] == '\t' || str[i+1] == '\0'))
             str[i] -= 32;
 
-        write(1, &str[i], 1);
+        write(1,&str[i],1);
         i++;
     }
-
 }
 
 int main(int argc, char **argv)
@@ -37,5 +29,5 @@ int main(int argc, char **argv)
         }
     }
     else
-        write(1,"\n",1);
+        write(1, "\n", 1);
 }
